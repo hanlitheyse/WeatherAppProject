35b6936b49db0ab9b62ad0733be05cbaebadc463
hanlitheyse <hanlitheyse@gmail.com>
2023-09-02 14:01:24 +1000

    api fixed

--- script.js
+++ script.js
@@ -15,27 +15,7 @@ function getWeatherCurrentLocation(lon, lat) {
   let apiNowUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
   let apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=249d758901864c4a8d893119233008&q=${lat},${lon}&days=7`;
   axios.get(apiUrl).then(forecastResponse);
-  axios
-    .get(apiNowUrl)
-    .then(weatherResponse)
-    .catch(function (error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
-    });
+  axios.get(apiNowUrl).then(weatherResponse).;
   searchLoc = "current";
 }
 
