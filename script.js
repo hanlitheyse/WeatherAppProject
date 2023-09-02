92209825bad6513d8c94cb915f51c819a2907367
hanlitheyse <hanlitheyse@gmail.com>
2023-09-02 22:24:20 +1000

    final

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 let apiKey = "5cf16f4aba42dad5b51c00ba4f512a46";
 let lon = "";
 let lat = "";
-let searchLoc = "";
-
+let curTemperature = "";
+let curMinTemperature = "";
+let curMaxTemperature = "";
+let tomMinTemperature = "";
+let tomMaxTemperature = "";
+let aTomMinTemperature = "";
+let aTomMaxTemperature = "";
+let aaTomMinTemperature = "";
+let aaTomMaxTemperature = "";
+let aaaTomMinTemperature = "";
+let aaaTomMaxTemperature = "";
 navigator.geolocation.getCurrentPosition(getLocation);
 
 function getLocation(position) {
@@ -13,25 +22,25 @@ function getLocation(position) {
 
 function getWeatherCurrentLocation(lon, lat) {
   let apiNowUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  let apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=249d758901864c4a8d893119233008&q=${lat},${lon}&days=3`;
+  let apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=249d758901864c4a8d893119233008&q=${lat},${lon}&days=4`;
   axios.get(apiUrl).then(forecastResponse);
   axios.get(apiNowUrl).then(weatherResponse);
-  searchLoc = "current";
 }
 
 function getWeather(location) {
   let apiNowUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
-  let apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=249d758901864c4a8d893119233008&q=${location}&days=3`;
+  let apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=249d758901864c4a8d893119233008&q=${location}&days=4`;
   axios.get(apiUrl).then(forecastResponse);
 
   axios.get(apiNowUrl).then(weatherResponse);
-  searchLoc = "new";
 }
 
 function weatherResponse(response) {
+  let id = response.data.weather[0].id;
+  setBackground(id);
+
   let icon = response.data.weather[0].icon;
   let iconSrc = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-  console.log(icon);
   let liveIcon = document.getElementById("icon");
   liveIcon.setAttribute("src", iconSrc);
 
@@ -48,6 +57,7 @@ function weatherResponse(response) {
   curTemp = Math.round(curTemp);
   let liveTemp = document.querySelector("p#liveTemp");
   liveTemp.innerHTML = `${curTemp}°C`;
+  curTemperature = curTemp;
 
   let minTemp = response.data.main.temp_min;
   minTemp = Math.round(minTemp);
@@ -55,6 +65,8 @@ function weatherResponse(response) {
   maxTemp = Math.round(maxTemp);
   let live_mmTemp = document.querySelector("p#live_mmTemp");
   live_mmTemp.innerHTML = `${maxTemp}°C | ${minTemp}°C`;
+  curMinTemperature = minTemp;
+  curMaxTemperature = maxTemp;
 
   let weather = response.data.weather[0].main;
   let liveWeather = document.querySelector("h1#weather");
@@ -74,6 +86,35 @@ function weatherResponse(response) {
   let humidity = response.data.main.humidity;
   let liveHumidity = document.querySelector("p#humidity");
   liveHumidity.innerHTML = `<b>Humidity: </b>${humidity}%`;
+}
+
+function setBackground(id) {
+  let liveBackground = document.getElementById("back");
+
+  if (id === 800) {
+    liveBackground.style.backgroundImage = "url(sunny_sky.jpg)";
+  } else {
+    const firstDigit = String(id).charAt(0);
+    switch (firstDigit) {
+      case "2":
+        liveBackground.style.backgroundImage = "url(stormy_sky.jpg)";
+        break;
+      case "3":
+        liveBackground.style.backgroundImage = "url(cloudy_sky.jpg) ";
+        break;
+      case "5":
+        liveBackground.style.backgroundImage = "url(rain_sky.jpg)";
+        break;
+      case "6":
+        liveBackground.style.backgroundImage = "url(snow.jpg)";
+        break;
+      case "7":
+        liveBackground.style.backgroundImage = "url(mist.jpg)";
+        break;
+      case "8":
+        liveBackground.style.backgroundImage = "url(cloudy_sky.jpg)";
+    }
+  }
 }
 
 function forecastResponse(response) {
@@ -99,7 +140,16 @@ function forecastResponse(response) {
   );
   let aa_tomWeather = response.data.forecast.forecastday[2].day.condition.text;
   aa_tomForecast(aa_tomMax, aa_tomMin, aa_tomWeather);
-  createIcon(tomWeather, a_tomWeather, aa_tomWeather);
+
+  let aaa_tomMax = Math.round(
+    response.data.forecast.forecastday[3].day.maxtemp_c
+  );
+  let aaa_tomMin = Math.round(
+    response.data.forecast.forecastday[3].day.mintemp_c
+  );
+  let aaa_tomWeather = response.data.forecast.forecastday[3].day.condition.text;
+  aaa_tomForecast(aaa_tomMax, aaa_tomMin, aaa_tomWeather);
+  createIcon(tomWeather, a_tomWeather, aa_tomWeather, aaa_tomWeather);
 }
 
 function tomForecast(max, min, weather) {
@@ -108,6 +158,8 @@ function tomForecast(max, min, weather) {
 
   live_temp.innerHTML = `<h3>${max}°C</h3>| ${min}°C`;
   live_weather.innerHTML = `${weather}`;
+  tomMinTemperature = min;
+  tomMaxTemperature = max;
 }
 
 function a_tomForecast(max, min, weather) {
@@ -116,6 +168,9 @@ function a_tomForecast(max, min, weather) {
 
   live_temp.innerHTML = `<h3>${max}°C</h3>| ${min}°C`;
   live_weather.innerHTML = `${weather}`;
+
+  aTomMinTemperature = min;
+  aTomMaxTemperature = max;
 }
 
 function aa_tomForecast(max, min, weather) {
@@ -124,13 +179,28 @@ function aa_tomForecast(max, min, weather) {
 
   live_temp.innerHTML = `<h3>${max}°C</h3>| ${min}°C`;
   live_weather.innerHTML = `${weather}`;
+
+  aaTomMinTemperature = min;
+  aaTomMaxTemperature = max;
 }
 
-function createIcon(tom, a_tom, aa_tom) {
+function aaa_tomForecast(max, min, weather) {
+  let live_temp = document.querySelector("li#aaa-temp");
+  let live_weather = document.querySelector("li#aaa-weather");
+
+  live_temp.innerHTML = `<h3>${max}°C</h3>| ${min}°C`;
+  live_weather.innerHTML = `${weather}`;
+
+  aaaTomMinTemperature = min;
+  aaaTomMaxTemperature = max;
+}
+
+function createIcon(tom, a_tom, aa_tom, aaa_tom) {
   console.log(a_tom, aa_tom);
   let liveTom = document.getElementById("tom");
   let live_aTom = document.getElementById("a_tom");
   let live_aaTom = document.getElementById("aa_tom");
+  let live_aaaTom = document.getElementById("aaa_tom");
 
   if (tom === "Sunny") {
     liveTom.style.backgroundImage = "url(sunny_sky.jpg)";
@@ -165,7 +235,7 @@ function createIcon(tom, a_tom, aa_tom) {
     tom === "Moderate or heavy sleet" ||
     tom === "Moderate or heavy showers of ice pellets"
   ) {
-    liveTom.style.backgroundImage = "url(rain.jpg)";
+    liveTom.style.backgroundImage = "url(rain_sky.jpg)";
   } else if (
     tom === "Thundery outbreaks possible" ||
     tom === "Moderate or heavy snow with thunder" ||
@@ -223,7 +293,7 @@ function createIcon(tom, a_tom, aa_tom) {
     a_tom === "Moderate or heavy sleet" ||
     a_tom === "Moderate or heavy showers of ice pellets"
   ) {
-    live_aTom.style.backgroundImage = "url(rain.jpg)";
+    live_aTom.style.backgroundImage = "url(rain_sky.jpg)";
   } else if (
     a_tom === "Thundery outbreaks possible" ||
     a_tom === "Moderate or heavy snow with thunder" ||
@@ -284,7 +354,7 @@ function createIcon(tom, a_tom, aa_tom) {
     aa_tom === "Moderate or heavy sleet" ||
     aa_tom === "Moderate or heavy showers of ice pellets"
   ) {
-    live_aaTom.style.backgroundImage = "url(rain.jpg)";
+    live_aaTom.style.backgroundImage = "url(rain_sky.jpg)";
   } else if (
     aa_tom === "Thundery outbreaks possible" ||
     aa_tom === "Moderate or heavy snow with thunder" ||
@@ -303,9 +373,70 @@ function createIcon(tom, a_tom, aa_tom) {
   } else if (
     aa_tom === "Patchy heavy snow" ||
     aa_tom === "Heavy snow" ||
-    a_tom === "Moderate or heavy snow showers"
+    aa_tom === "Moderate or heavy snow showers"
   ) {
     live_aaTom.style.backgroundImage = "url(snow).jpg)";
+  }
+
+  if (aaa_tom === "Sunny") {
+    live_aaaTom.style.backgroundImage = "url(sunny_sky.jpg)";
+  } else if (
+    aaa_tom === "Cloudy" ||
+    aaa_tom === "Overcast" ||
+    aaa_tom === "Light drizzle"
+  ) {
+    live_aaaTom.style.backgroundImage = "url(cloudy_sky.jpg)";
+  } else if (aaa_tom === "Partly cloudy") {
+    live_aaaTom.style.backgroundImage = "url(mostly_sunny.jpg)";
+  } else if (
+    aaa_tom === "Mist" ||
+    aaa_tom === "Fog" ||
+    aaa_tom === "Freezing fog"
+  ) {
+    live_aaaTom.style.backgroundImage = "url(mist.jpg)";
+  } else if (
+    aaa_tom === "Patchy rain possible" ||
+    aaa_tom === "Patchy light rain" ||
+    aaa_tom === "Light rain" ||
+    aaa_tom === "Moderate rain at times" ||
+    aaa_tom === "Moderate rain" ||
+    aaa_tom === "Light freezing rain"
+  ) {
+    live_aaaTom.style.backgroundImage = "url(lightRain.jpg)";
+  } else if (
+    aaa_tom === "Patchy sleet possible" ||
+    aaa_tom === "Patchy freezing drizzle possible" ||
+    aaa_tom === "Freezing drizzle" ||
+    aaa_tom === "Heavy freezing drizzle" ||
+    aaa_tom === "Heavy rain at times" ||
+    aaa_tom === "Heavy rain" ||
+    aaa_tom === "Moderate or heavy freezing rain" ||
+    aaa_tom === "Light sleet" ||
+    aaa_tom === "Moderate or heavy sleet" ||
+    aaa_tom === "Moderate or heavy showers of ice pellets"
+  ) {
+    live_aaaTom.style.backgroundImage = "url(rain_sky.jpg)";
+  } else if (
+    aaa_tom === "Thundery outbreaks possible" ||
+    aaa_tom === "Moderate or heavy snow with thunder" ||
+    aaa_tom === "Patchy light snow with thunder" ||
+    aaa_tom === "Moderate or heavy rain with thunder" ||
+    aaa_tom === "Patchy light rain with thunder"
+  ) {
+    live_aaaTom.style.backgroundImage = "url(stormy_sky.jpg)";
+  } else if (
+    aaa_tom === "Patchy light snow" ||
+    aaa_tom === "Light snow" ||
+    aaa_tom === " Patchy moderate snow" ||
+    aaa_tom === "Moderate snow"
+  ) {
+    live_aaaTom.style.backgroundImage = "url(patchySnow).jpg)";
+  } else if (
+    aaa_tom === "Patchy heavy snow" ||
+    aaa_tom === "Heavy snow" ||
+    aaa_tom === "Moderate or heavy snow showers"
+  ) {
+    live_aaaTom.style.backgroundImage = "url(snow).jpg)";
   }
 }
 
@@ -348,14 +479,73 @@ function checkToggle(btn) {
 }
 
 function toggleTemp(unit) {
+  let live_curTemperature = document.querySelector("p#liveTemp");
+  let live_curMMTemperature = document.querySelector("p#live_mmTemp");
+  let live_tomTemperature = document.querySelector("li#tom-temp");
+  let live_aTomTemperature = document.querySelector("li#a-temp");
+  let live_aaTomTemperature = document.querySelector("li#aa-temp");
+  let live_aaaTomTemperature = document.querySelector("li#aaa-temp");
 
+  console.log(curTemperature);
   switch (unit) {
-    case "c":
-      liveTemp.innerHTML = "29°C";
-      break;
     case "f":
-      
-;
+      curTemperature = celToFar(curTemperature);
+      curMaxTemperature = celToFar(curMaxTemperature);
+      curMinTemperature = celToFar(curMinTemperature);
+
+      tomMinTemperature = celToFar(tomMinTemperature);
+      tomMaxTemperature = celToFar(tomMaxTemperature);
+
+      aTomMinTemperature = celToFar(aTomMinTemperature);
+      aTomMaxTemperature = celToFar(aTomMaxTemperature);
+
+      aaTomMinTemperature = celToFar(aaTomMinTemperature);
+      aaTomMaxTemperature = celToFar(aaTomMaxTemperature);
+
+      aaaTomMaxTemperature = celToFar(aaaTomMaxTemperature);
+      aaaTomMinTemperature = celToFar(aaaTomMinTemperature);
+
+      live_curTemperature.innerHTML = `${curTemperature}°F`;
+      live_curMMTemperature.innerHTML = `${curMaxTemperature}°F | ${curMinTemperature}°F`;
+      live_tomTemperature.innerHTML = `<h3>${tomMaxTemperature}°F</h3>| ${tomMinTemperature}°F`;
+      live_aTomTemperature.innerHTML = `<h3>${aTomMaxTemperature}°F</h3>| ${aTomMinTemperature}°F`;
+      live_aaTomTemperature.innerHTML = `<h3>${aaTomMaxTemperature}°F</h3>| ${aaTomMinTemperature}°F`;
+      live_aaaTomTemperature.innerHTML = `<h3>${aaaTomMaxTemperature}°F</h3>| ${aaaTomMinTemperature}°F`;
+
+      break;
+    case "c":
+      curTemperature = farToCel(curTemperature);
+      curMaxTemperature = farToCel(curMaxTemperature);
+      curMinTemperature = farToCel(curMinTemperature);
+
+      tomMinTemperature = farToCel(tomMinTemperature);
+      tomMaxTemperature = farToCel(tomMaxTemperature);
+
+      aTomMinTemperature = farToCel(aTomMinTemperature);
+      aTomMaxTemperature = farToCel(aTomMaxTemperature);
+
+      aaTomMinTemperature = farToCel(aaTomMinTemperature);
+      aaTomMaxTemperature = farToCel(aaTomMaxTemperature);
+
+      aaaTomMinTemperature = farToCel(aaaTomMinTemperature);
+      aaaTomMaxTemperature = farToCel(aaaTomMaxTemperature);
+
+      live_curTemperature.innerHTML = `${curTemperature}°C`;
+      live_curMMTemperature.innerHTML = `${curMaxTemperature}°C | ${curMinTemperature}°C`;
+      live_tomTemperature.innerHTML = `<h3>${tomMaxTemperature}°C</h3>| ${tomMinTemperature}°C`;
+      live_aTomTemperature.innerHTML = `<h3>${aTomMaxTemperature}°C</h3>| ${aTomMinTemperature}°C`;
+      live_aaTomTemperature.innerHTML = `<h3>${aaTomMaxTemperature}°C</h3>| ${aaTomMinTemperature}°C`;
+      live_aaaTomTemperature.innerHTML = `<h3>${aaaTomMaxTemperature}°C</h3>| ${aaaTomMinTemperature}°C`;
+
       break;
   }
+  console.log(curTemperature);
+}
+
+function celToFar(temp) {
+  return Math.round((temp * 9) / 5 + 32);
+}
+
+function farToCel(temp) {
+  return Math.round(((temp - 32) * 5) / 9);
 }
